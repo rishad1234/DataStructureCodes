ece2b2c5c6e779cf53ba927c166ec02d4c3ab7ef
Rishadul Islam <rishadulislamkhan@gmail.com>
2018-06-01 19:41:22 +0600

    circular linked list
    doubly type
    1st version

---
+++ CircularLinkedList/src/circularlinkedlist/CircularList.java
@@ -0,0 +1,82 @@
+
+package circularlinkedlist;
+
+public class CircularList {
+    private Node head;
+    private Node tail;
+
+    public CircularList() {
+        head = new Node(Integer.MIN_VALUE);
+        tail = new Node(Integer.MAX_VALUE);
+        head.setPrevious(tail);
+        tail.setNext(head);
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public void setHead(Node head) {
+        this.head = head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public void setTail(Node tail) {
+        this.tail = tail;
+    }
+    
+    public void insert(int number){
+        Node node  = new Node(number);
+        if(head.getNext() == null && tail.getPrevious() == null){
+            head.setNext(node);
+            node.setPrevious(head);
+            node.setNext(tail);
+            tail.setPrevious(node);
+        }else {
+            Node current = head.getNext();
+            while(current != tail && node.getNumber() > current.getNumber()){
+                current = current.getNext();
+            }
+            node.setPrevious(current.getPrevious());
+            node.setNext(current);
+            current.getPrevious().setNext(node);
+            current.setPrevious(node);
+        }
+    }
+    
+    public void delete(int number){
+        Node current = head.getNext();
+        while(current != tail){
+            Node temp;
+            if(number == current.getNumber()){
+                temp = current;
+                while(current != tail && number == current.getNumber()){
+                    current = current.getNext();
+                }
+                temp.getPrevious().setNext(current);
+                current.setPrevious(temp.getPrevious());
+            }else{
+                current = current.getNext();
+            }
+        }
+    }
+    
+    public void printListForward(){
+        Node current = head.getNext();
+        while(current != tail){
+            System.out.println(current);
+            current = current.getNext();
+        }
+    }
+    public void printListBackward(){
+        Node current = tail.getPrevious();
+        while(current != head){
+            System.out.println(current);
+            current = current.getPrevious();
+        }
+    }
+    
+}
---
+++ CircularLinkedList/src/circularlinkedlist/Node.java
@@ -0,0 +1,42 @@
+
+package circularlinkedlist;
+
+public class Node {
+    private int number;
+    private Node previous;
+    private Node next;
+
+    public Node(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Node getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(Node previous) {
+        this.previous = previous;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" + "number=" + number + '}';
+    }
+    
+}
