4b5af39f11c32a9f836f82ac9fce5e15dacd99d3
Rishadul Islam <rishadulislamkhan@gmail.com>
2018-07-08 20:18:47 +0600

    Hanoi recursive

---
+++ HanoiRecursive/src/hanoirecursive/HanoiRecursive.java
@@ -0,0 +1,18 @@
+package hanoirecursive;
+
+public class HanoiRecursive {
+
+    public static void main(String[] args) {
+       hanoi(20, 1, 2, 3);
+    }
+    
+    public static void hanoi(int disk, int i, int j, int k){
+        if(disk == 1){
+            System.out.println(i + "->" + k);
+        }else{
+            hanoi(disk-1, i, k, j);
+            hanoi(1, i, j, k);
+            hanoi(disk-1, j, i, k);
+        }
+    }
+}
