9d134535691fd2f838b7ef04bea53915ae63ab8c
Rishadul Islam <rishadulislamkhan@gmail.com>
2018-07-15 17:31:16 +0600

    user input initiated

--- InfixToPostfixTest/src/infixtopostfixtest/InfixToPostfixTest.java
+++ InfixToPostfixTest/src/infixtopostfixtest/InfixToPostfixTest.java
@@ -1,13 +1,18 @@
 
 package infixtopostfixtest;
 
+import java.util.Scanner;
+
 public class InfixToPostfixTest {
 
     public static void main(String[] args) {
         /*
             its very important to put spaces between the characters
         */
-        InfixToPostfix p = new InfixToPostfix("1233 + 2 ^ 3 + 3 * 4");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the String(with white spaces between every character): ");
+        
+        InfixToPostfix p = new InfixToPostfix(input.nextLine());
         System.out.println(p.toPostfix());
         System.out.println(p.evaluatePostFix());
     }
