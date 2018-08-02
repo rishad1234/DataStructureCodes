4fe79a2fe7a2f0951e7579609c4bdff5c5fa748c
Rishadul Islam <rishadulislamkhan@gmail.com>
2018-08-02 15:56:47 +0600

    insert methods are added, no bugs so far

--- BinarysearchTreeTest/src/binarysearchtreetest/BinarySearchTree.java
+++ BinarysearchTreeTest/src/binarysearchtreetest/BinarySearchTree.java
@@ -1,6 +1,8 @@
 
 package binarysearchtreetest;
 
+import java.util.Stack;
+
 public class BinarySearchTree {
     private Node root;
 
@@ -82,6 +84,102 @@ public class BinarySearchTree {
             return temp.getValue();
         }else{
             return max(temp.getRight());
+        }
+    }
+    
+    public void preOrderRecursive(Node root){
+        if(root == null){
+            
+        }else{
+            System.out.println(root.getValue() + ": parent: " + root.getParent());
+            preOrderRecursive(root.getLeft());
+            preOrderRecursive(root.getRight());
+        }
+    }
+    
+    public void inOrderRecursive(Node root){
+        if(root == null){
+            
+        }else{
+            inOrderRecursive(root.getLeft());
+            System.out.println(root.getValue() + ": parent: " + root.getParent());
+            inOrderRecursive(root.getRight());
+        }
+    }
+    
+    public void postOrderRecursive(Node root){
+        if(root != null){
+            postOrderRecursive(root.getLeft());
+            postOrderRecursive(root.getRight());
+            System.out.println(root.getValue() + ": parent: " + root.getParent());
+        }
+    }
+    
+    public void preOrderNonRecursive(Node root){
+        Stack<Node> stack = new Stack<>();
+        Node node = root;
+        stack.push(node);
+        while(!stack.isEmpty()){
+            Node temp = stack.pop();
+            System.out.println(temp.getValue() + ": parent: " + temp.getParent());
+            if(temp.getRight() != null){
+                stack.push(temp.getRight());
+            }
+            if(temp.getLeft()!= null){
+                stack.push(temp.getLeft());
+            }
+        }
+    }
+
+    public void inOrderNonRecursive(Node root){
+        Stack<Node> stack = new Stack<>();
+        if(root == null){
+            System.out.println(root);
+        }else{
+            Node node = root;
+            while(node != null){
+                stack.push(node);
+                node = node.getLeft();
+            }
+        }
+        
+        while(!stack.isEmpty()){
+            Node node = stack.pop();
+            System.out.println(node.getValue() + ": parent: " + node.getParent());
+            if(node.getRight() != null){
+                node = node.getRight();  
+                while(node != null){
+                    stack.push(node);
+                    node = node.getLeft();
+                }
+            }
+        }
+    }
+    
+    public void postOrderNonRecursive(Node root){
+        if(root != null){
+            Stack<Node> stack = new Stack<>();
+            Stack<Node> temp = new Stack<>();
+            
+            temp.push(root);
+            Node node = null;
+            
+            while(!temp.isEmpty()){
+                node = temp.pop();
+                stack.push(node);
+                
+                if(node.getLeft() != null){
+                    temp.push(node.getLeft());
+                }
+                if(node.getRight() != null){
+                    temp.push(node.getRight());
+                }
+            }
+            
+            while(!stack.isEmpty()){
+                Node n = stack.pop();
+                System.out.println(n.getValue() + ": parent: " + n.getParent());
+            }
         }
     }
 }
--- BinarysearchTreeTest/src/binarysearchtreetest/BinarysearchTreeTest.java
+++ BinarysearchTreeTest/src/binarysearchtreetest/BinarysearchTreeTest.java
@@ -6,9 +6,11 @@ public class BinarysearchTreeTest {
     public static void main(String[] args) {
         BinarySearchTree btree = new BinarySearchTree();
         
-        for(int i = 1; i <= 100; i+=4){
+        for(int i = 1; i <= 100; i+=1){
             btree.insertBST(i);
         }
+        
+        btree.postOrderRecursive(btree.getRoot());
     }
     
 }
